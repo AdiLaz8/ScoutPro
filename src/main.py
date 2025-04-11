dc919f65702a99c35cc45e6259a6c1615c45fa83
Adi Lazarovici <lazarovici@mail.tau.ac.il>
2025-04-11 12:26:25 +0300

    .

--- src/main.py
+++ src/main.py
@@ -16,6 +16,10 @@ def get_relative_path(filename):
 players_df = processing.filter_and_process_players(get_relative_path('players.csv'))
 attributes_df = processing.load_prepare_attributes(get_relative_path('male_players.csv'))
 merged_df = processing.merge_players_and_attributes(players_df, attributes_df)
-print(merged_df.head())
-print("Total rows in the DataFrame:", merged_df.shape[0])
-print(merged_df.columns.tolist())
+player_stats_df = processing.summarize_player_statistics(get_relative_path('appearances.csv'))
+final_df = processing.merge_with_appearances(merged_df, player_stats_df)
+# הדפסת 40 השחקנים הראשונים מה-DataFrame הסופי
+print(final_df.head(40))
+
+# הדפסת מספר השורות והעמודות ב-DataFrame הסופי
+print("Number of rows and columns in the final DataFrame:", final_df.shape)
--- src/processing.py
+++ src/processing.py
@@ -78,12 +78,41 @@ def merge_players_and_attributes(players_df: pd.DataFrame, attributes_df: pd.Dat
     attributes_df["name"] = attributes_df["name"].str.strip().str.lower()
     players_df = players_df.drop_duplicates(subset=["name"])
     attributes_df = attributes_df.drop_duplicates(subset=["name"])
-
-    
     merged_df = pd.merge(players_df, attributes_df, on="name", how="inner")
-
-    print(f"Merged dataframe contains {merged_df.shape[0]} players with full data.")
     merged_df.columns = [col.replace("_", " ") for col in merged_df.columns]
-
     return merged_df
+
+def summarize_player_statistics(file_path):
+    # טעינת הנתונים מהקובץ CSV
+    df = pd.read_csv(file_path)
+    
+    # בדיקה שהעמודות קיימות
+    if 'player_id' in df.columns and 'goals' in df.columns and 'assists' in df.columns and 'date' in df.columns:
+        # המרת עמודת התאריך לפורמט datetime
+        df['date'] = pd.to_datetime(df['date'], errors='coerce')
+        
+        # טווח התאריכים לסינון
+        start_date = pd.Timestamp('2023-07-01')
+        end_date = pd.Timestamp('2024-07-01')
+        
+        # סינון השורות לפי התאריכים
+        df = df[(df['date'] >= start_date) & (df['date'] < end_date)]
+        
+        # קבוצה לפי player_id וסכימת הגולים והבישולים
+        summary_df = df.groupby('player_id')[['goals', 'assists']].sum().reset_index()
+        
+        # החלפת ערכים חסרים ב-0
+        summary_df[['goals', 'assists']] = summary_df[['goals', 'assists']].fillna(0)
+    else:
+        print("One or more columns are missing in the dataframe.")
+        return None
+    return summary_df
+
+def merge_with_appearances(merged_df: pd.DataFrame, appearances_df: pd.DataFrame) -> pd.DataFrame:
+
+    merged_full = pd.merge(merged_df, appearances_df, on="player_id", how="left")
+    merged_full["goals"] = merged_full["goals"].fillna(0).astype(int)
+    merged_full["assists"] = merged_full["assists"].fillna(0).astype(int)
+
+    return merged_full
 
